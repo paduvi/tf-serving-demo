08fb4102e1993082786a2c396b6e0401dde9360e
vietpd <viet3695@gmail.com>
2017-11-07 01:13:26 +0700

    update

--- model/model.py
+++ model/model.py
@@ -19,16 +19,12 @@ builder = tf.saved_model.builder.SavedModelBuilder(export_path)
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
-    tensor_info_x = tf.saved_model.utils.build_tensor_info(x)
-    tensor_info_y = tf.saved_model.utils.build_tensor_info(y)
-    tensor_info_z = tf.saved_model.utils.build_tensor_info(z)
-
     builder.add_meta_graph_and_variables(
         sess,
         [tf.saved_model.tag_constants.SERVING],
         signature_def_map={
             "magic_model": tf.saved_model.signature_def_utils.predict_signature_def(
-                inputs={"egg": tensor_info_x, "bacon": tensor_info_y},
-                outputs={"spam": tensor_info_z})
+                inputs={"egg": x, "bacon": y},
+                outputs={"spam": z})
         })
     builder.save()

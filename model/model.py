c1cc290df063415f1bdec697089eced203d002ea
vietpd <viet3695@gmail.com>
2017-11-07 01:11:10 +0700

    update new version

--- model/model.py
+++ model/model.py
@@ -1,26 +1,34 @@
 import tensorflow as tf
 import os
-from tensorflow.python.util import compat
 
-x = tf.placeholder(tf.float32, shape=(None))
-y = tf.placeholder(tf.float32, shape=(None))
+x = tf.placeholder(tf.float32, shape=None)
+y = tf.placeholder(tf.float32, shape=None)
 
-three = tf.Variable(3, dtype= tf.float32)
+three = tf.Variable(3, dtype=tf.float32)
 z = tf.scalar_mul(three, x) + y
 
 model_version = 1
-path = os.path.join("three_x_plus_y", str(model_version))
-builder = tf.python.saved_model.builder.SavedModelBuilder(path)
+export_path_base = "three_x_plus_y"
+export_path = os.path.join(
+    tf.compat.as_bytes(export_path_base),
+    tf.compat.as_bytes(str(model_version)))
+print 'Exporting trained model to', export_path
+
+builder = tf.saved_model.builder.SavedModelBuilder(export_path)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
+    tensor_info_x = tf.saved_model.utils.build_tensor_info(x)
+    tensor_info_y = tf.saved_model.utils.build_tensor_info(y)
+    tensor_info_z = tf.saved_model.utils.build_tensor_info(z)
+
     builder.add_meta_graph_and_variables(
         sess,
-        [tf.python.saved_model.tag_constants.SERVING],
-        signature_def_map= {
+        [tf.saved_model.tag_constants.SERVING],
+        signature_def_map={
             "magic_model": tf.saved_model.signature_def_utils.predict_signature_def(
-                inputs= {"egg": x, "bacon": y},
-                outputs= {"spam": z})
+                inputs={"egg": tensor_info_x, "bacon": tensor_info_y},
+                outputs={"spam": tensor_info_z})
         })
     builder.save()
